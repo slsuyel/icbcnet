a59e8a7ef5c80b49f951a0e30a0372edd01156f6
slsuyel <slsuyel@gmail.com>
2024-10-13 21:29:12 +0600

    changeedf

--- resources/views/frontlayout.blade.php
+++ resources/views/frontlayout.blade.php
@@ -76,6 +76,6 @@ console.log("{{ Auth::user() }}")
         </script>
     <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}" ></script>
     <script src="{{ asset('frontend/js/bootstrap.min.js') }}" ></script>
-    <script src="{{ asset('js/frontend.js?ver=1.0.4') }}"></script>
+    <script src="{{ asset('js/frontend.js?ver=1.0.5') }}"></script>
 </body>
 </html>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -86,7 +86,7 @@
 
 
 
-<script src="{{ asset('js/backend.js?ver=1.0.4') }}"></script>
+<script src="{{ asset('js/backend.js?ver=1.0.5') }}"></script>
 
 
 
